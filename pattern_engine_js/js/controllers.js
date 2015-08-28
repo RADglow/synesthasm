b5478d6ab3d5aada95ecf7d087e0d46b7cdb680d
Girts <girts.folkmanis@gmail.com>
2015-08-29 00:35:50 +0100

    Quick hacky way to animate a whole strip.

--- pattern_engine_js/js/controllers.js
+++ pattern_engine_js/js/controllers.js
@@ -20,20 +20,6 @@ patternApp.controller('PatternAppCtrl', function ($scope, $interval) {
         }
     })
 
-    $scope.startAnimation = function () {
-        if (angular.isDefined(stop)) return;
-        stop = $interval(function () {
-            $scope.animate(new Date)
-        }, 10);
-    };
-
-    $scope.stopAnimation = function () {
-        if (angular.isDefined(stop)) {
-            $interval.cancel(stop);
-            stop = undefined;
-        }
-    };
-
     $scope.animate = function (elapsedMs) {
         angular.forEach($scope.strips, function (strip, stripNo) {
             angular.forEach(strip.pixels, function (pixel, pixelNo) {
@@ -68,7 +54,8 @@ patternApp.controller('PatternAppCtrl', function ($scope, $interval) {
     */
 });
 
-patternApp.controller('BytecodeCtrl', function ($scope) {
+// XXX: split out byte code controller from strip controller.
+patternApp.controller('BytecodeCtrl', function ($scope, $interval) {
   $scope.progData = {
     asmInput: (
         '  // Are we on pixel zero?\n' +
@@ -91,6 +78,7 @@ patternApp.controller('BytecodeCtrl', function ($scope) {
         '  DIV R0, R0, COUNT\n' +
         '  WHSL R0, 255, 128\n'),
     assembled: [],
+    maxAddress: 0,
     state: new pattern_engine.State(),
   };
 
@@ -115,6 +103,7 @@ patternApp.controller('BytecodeCtrl', function ($scope) {
     $scope.errors = '';
     $scope.reset();
     $scope.progData.assembled = [];
+    $scope.progData.maxAddress = 0;
     try {
       $scope.progData.assembled = pattern_engine.assemble(
           $scope.progData.asmInput);
@@ -122,32 +111,56 @@ patternApp.controller('BytecodeCtrl', function ($scope) {
       $scope.errors = e.toString();
       throw e;
     }
+    $.each($scope.progData.assembled, function(_, item) {
+      if (item.address && item.address > $scope.progData.maxAddress) {
+        $scope.progData.maxAddress = item.address;
+      }
+    });
   };
 
-  $scope.reset = function () {
+  $scope.reset = function() {
     $scope.progData.state = new pattern_engine.State();
+    $scope.strip.currentPixel = 0;
+    $scope.strip.currentTick = 0;
+    $scope.setSpecialRegisters();
+    $scope.strip.pixels = [];
+    for (var i = 0; i < $scope.strip.pixelCount; i++) {
+      $scope.strip.pixels.push(jQuery.Color(0, 0, 0));
+    }
   };
+
+  $scope.setSpecialRegisters = function() {
+    $scope.progData.state.s[0] = $scope.strip.currentTick;
+    $scope.progData.state.s[1] = $scope.strip.currentPixel;
+    $scope.progData.state.s[2] = $scope.strip.pixelCount;
+  }
 
   var pixelHandler = {
     setRgb: function(r, g, b) {
-      $scope.pixel.r = r; $scope.pixel.g = g; $scope.pixel.b = b;
-      $scope.pixel.h = 0; $scope.pixel.s = 0; $scope.pixel.l = 0;
-      $scope.pixelStyle['background-color'] = jQuery.Color({
+      var color = jQuery.Color({
         red: r, green: g, blue: b, alpha: 1,
       });
+      $scope.strip.pixels[$scope.strip.currentPixel] = color;
+      $scope.pixel.r = r; $scope.pixel.g = g; $scope.pixel.b = b;
+      $scope.pixel.h = 0; $scope.pixel.s = 0; $scope.pixel.l = 0;
+      $scope.pixelStyle['background-color'] = color;
     },
     setHsl: function(h, s, l) {
-      $scope.pixel.r = 0; $scope.pixel.g = 0; $scope.pixel.b = 0;
-      $scope.pixel.h = h; $scope.pixel.s = s; $scope.pixel.l = l;
-      $scope.pixelStyle['background-color'] = jQuery.Color({
+      var color = jQuery.Color({
         hue: 360.0 * h / 255.0,
         saturation: s / 255.0,
         lightness: l / 255.0,
         alpha: 1
       });
+      $scope.strip.pixels[$scope.strip.currentPixel] = color;
+      $scope.pixel.r = 0; $scope.pixel.g = 0; $scope.pixel.b = 0;
+      $scope.pixel.h = h; $scope.pixel.s = s; $scope.pixel.l = l;
+      $scope.pixelStyle['background-color'] = color;
     },
   };
 
+  // Performs one step of the program. Returns true if program is not finished
+  // yet for the current pixel.
   $scope.step = function () {
     var pc = $scope.progData.state.pc;
     $.each($scope.progData.assembled, function(_, item) {
@@ -156,26 +169,69 @@ patternApp.controller('BytecodeCtrl', function ($scope) {
             $scope.progData.state, pixelHandler);
       }
     });
+    if ($scope.progData.state.pc > $scope.progData.maxAddress) {
+      $scope.progData.state.pc = 0;
+      var pixel = $scope.strip.currentPixel + 1;
+      if (pixel >= $scope.strip.pixelCount) {
+        pixel = 0;
+      }
+      $scope.strip.currentPixel = pixel;
+      $scope.setSpecialRegisters();
+      return false;
+    }
+    return true;
   };
 
-  $scope.run = function() {
-    var maxPc = 0;
-    $.each($scope.progData.assembled, function(_, item) {
-      if (item.address && item.address > maxPc) {
-        maxPc = item.address;
-      }
-    });
+  $scope.runPixel = function() {
     for (var i = 0; i < 10000; i++) {
-      if ($scope.progData.state.pc > maxPc) {
-        $scope.progData.state.pc = 0;
+      if (!$scope.step()) {
         return;
       }
-      $scope.step();
     }
     throw new Error('Program did not finish within 10000 cycles.');
   };
 
+  $scope.runFrame = function() {
+    for (;;) {
+      $scope.runPixel();
+      if ($scope.strip.currentPixel == 0) {
+        $scope.strip.currentTick += 100;
+        return;
+      }
+    }
+  };
+
+  $scope.strip = {
+    pixelCount: 24,
+    currentPixel: 0,
+    currentTick: 0,
+    pixels: [],
+  };
+
+  $scope.changePixelCount = function() {
+    $scope.reset();
+  };
+
+  var animation = null;
+
+  $scope.startAnimation = function() {
+      if (animation !== null) {
+        return;
+      }
+      animation = $interval(function() {
+        $scope.runFrame();
+      }, 100);
+  };
+
+  $scope.stopAnimation = function() {
+    if (animation !== null) {
+      $interval.cancel(animation);
+      animation = null;
+    }
+  };
+
   $scope.assemble();
+  $scope.changePixelCount();
 });
 
 patternApp.filter('getbit', function () {
